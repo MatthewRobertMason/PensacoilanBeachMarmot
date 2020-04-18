6ba1c953eeae12122cc7b817bd570b4b2c8a870f
MatthewRobertMason <Git@matthewmason.ca>
2020-04-18 18:34:01 -0300

    plant class grid stuff

--- Assets/Scripts/YourPlant.cs
+++ Assets/Scripts/YourPlant.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class YourPlant : MonoBehaviour
 {
     public string PlantName;
+    
+    [System.Serializable]
+    public class PlantPart
+    {
+        public int x;
+        public int y;
+    }
 
+    public int plantGridSizeX = 19;
+    public int plantGridSizeY = 10;
 
+    public PlantPart[,] plantGrid;
+    public PlantPart plantPot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        plantGrid = new PlantPart[plantGridSizeX, plantGridSizeY];
+        plantPot = new PlantPart() { x = plantGridSizeX / 2, y = 0 };
+        plantGrid[plantPot.x, plantPot.y] = plantPot;
+        PlantPart pp = new PlantPart();
     }
 
     // Update is called once per frame
