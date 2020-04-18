3f4b58216962ddec01bb7a1829b5e9bb4748d75f
MatthewRobertMason <Git@matthewmason.ca>
2020-04-18 20:37:54 -0300

    Plant grow code

---
+++ Assets/Scripts/PlantTiles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[System.Serializable]
+public class PlantTiles
+{
+    public string description = null;
+
+    public bool isFoliage = false;
+
+    public bool pointsUp = false;
+    public bool pointsRight = false;
+    public bool pointsDown = false;
+    public bool pointsLeft = false;
+    
+    public bool isPlantPot = false;
+     
+    public UnityEngine.Tilemaps.Tile tile = null;
+}
--- Assets/Scripts/YourPlant.cs
+++ Assets/Scripts/YourPlant.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class YourPlant : MonoBehaviour
 {
@@ -11,26 +12,47 @@ public class YourPlant : MonoBehaviour
     {
         public int x;
         public int y;
-    }
 
-    public int plantGridSizeX = 19;
-    public int plantGridSizeY = 10;
+        public PlantTiles plantTile = null;
+    }
+    
+    public int plantGridSizeX = 17;
+    public int plantGridSizeY = 9;
 
     public PlantPart[,] plantGrid;
-    public PlantPart plantPot;
+    private PlantPart plantPot;
+
+    public UnityEngine.Tilemaps.Tilemap plantPotTileMap = null;
+    public UnityEngine.Tilemaps.Tilemap plantBranchTileMap = null;
+    public UnityEngine.Tilemaps.Tilemap plantFoliageTileMap = null;
+
+    private PlantTileCollection plantTileCollection = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        plantTileCollection = (PlantTileCollection)FindObjectOfType<PlantTileCollection>();
+
         plantGrid = new PlantPart[plantGridSizeX, plantGridSizeY];
-        plantPot = new PlantPart() { x = plantGridSizeX / 2, y = 0 };
+        plantPot = new PlantPart() { x = 8, y = 0, plantTile = plantTileCollection.plantTiles[0] };
         plantGrid[plantPot.x, plantPot.y] = plantPot;
         PlantPart pp = new PlantPart();
+
+        plantPotTileMap.SetTile(new Vector3Int(plantPot.x, plantPot.y, 0), plantPot.plantTile.tile);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public void GrowPlant()
+    {
+
+    }
+
+    public void ShrinkPlant()
+    {
+
     }
 }
