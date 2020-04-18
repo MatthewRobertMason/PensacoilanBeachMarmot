400703aebd58758acd395dac74bed95a6ca669d8
Adam Douglass <douglass@malloc.ca>
2020-04-18 12:49:23 -0400

    Check in stub files

---
+++ Assets/Scripts/YourPlant.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YourPlant : MonoBehaviour
+{
+    public string PlantName;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
