549e1f8ed514a8a543119f70c45a23bd475fdba9
Adam Douglass <douglass@malloc.ca>
2020-04-18 17:40:16 -0400

    Work on popups

--- Assets/Scripts/YourPlant.cs
+++ Assets/Scripts/YourPlant.cs
@@ -6,17 +6,27 @@ public class YourPlant : MonoBehaviour
 {
     public string PlantName;
 
+    private static YourPlant the_plant;
+    public static YourPlant GetInstancePlant()
+    {
+        return the_plant;
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        the_plant = this;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ApplyInteration(ObjectInteraction interaction)
+    {
+        Debug.Log("Did Thing");
     }
 }
