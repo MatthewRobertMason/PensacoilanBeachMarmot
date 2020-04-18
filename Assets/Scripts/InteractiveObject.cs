57070a8f41b7a04b59407e3d54708014cbb8f651
Adam Douglass <douglass@malloc.ca>
2020-04-18 14:04:33 -0400

    Add scripts and prefab

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -1,9 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct ObjectInteraction
+{
+    public string InteractionDescription;
+    public int Attention;
+    public int Hunger;
+    public int Thirst;
+    public int Stimulation;
+    public int Knowledge;
+    public int Reassurance;
+    public int Peace;
+}
+
 
 public class InteractiveObject : MonoBehaviour
 {
+    public string InteractionHeader;
+    public ObjectInteraction[] Interations;
+
+    public GameObject PopupPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +34,11 @@ public class InteractiveObject : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        GameObject popup = Instantiate(PopupPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        popup.GetComponent<Popup>().SetHeader(InteractionHeader);
     }
 }
---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup : MonoBehaviour
+{
+    public GameObject Header;
+
+    public void Remove()
+    {
+        Object.Destroy(this.gameObject);
+    }
+
+    public void SetHeader(string text)
+    {
+        Header.GetComponent<Text>().text = text;
+    }
+}
