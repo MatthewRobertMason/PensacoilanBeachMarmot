d55f47074cd70a192d107e4e1ea346a6fd1ac780
Adam Douglass <douglass@malloc.ca>
2020-04-19 17:09:27 -0400

    Make the mask mouse follow

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = new Vector3(position.x, position.y, 0);
+    }
+}
