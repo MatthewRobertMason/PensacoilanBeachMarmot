c3cc16531cbf0ccc6a800cb417fe8bffe18dd8bb
Adam Douglass <douglass@malloc.ca>
2020-04-20 14:41:52 -0400

    Bugfix

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -30,7 +30,7 @@ public class InteractiveObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        popupOpen = false;
     }
 
     // Update is called once per frame
