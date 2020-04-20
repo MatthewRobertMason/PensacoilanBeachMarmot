01fb983eb57b50fae3642c11b9eb6a4b13a4ad5b
Adam Douglass <douglass@malloc.ca>
2020-04-19 23:56:05 -0400

    Add more scenes, add cursors for action spaces

---
+++ Assets/Scripts/ButtonActions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonActions : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit(0);
+    }
+
+    public void StartEasy()
+    {
+        SceneManager.LoadScene("Appartment");
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
